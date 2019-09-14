7bf7852b23dec15b9418e0ebe25cc2744f8a8d7c
Lakhan Singh <lakhans671@gmail.com>
2019-09-15 02:07:40 +0400

    logs

--- src/main/java/com/os/biz/FluxApplication.java
+++ src/main/java/com/os/biz/FluxApplication.java
@@ -3,6 +3,7 @@ package com.os.biz;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
 import com.mongodb.async.client.MongoClient;
@@ -11,6 +12,7 @@ import com.mongodb.async.client.MongoClients;
 @SpringBootApplication
 //@ComponentScan("com.os.biz")
 @EnableReactiveMongoRepositories
+@EnableAspectJAutoProxy
 public class FluxApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/os/biz/repository/LoggingInMemoryHttpTraceRepository.java
+++ src/main/java/com/os/biz/repository/LoggingInMemoryHttpTraceRepository.java
@@ -1,5 +1,7 @@
 package com.os.biz.repository;
 import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.actuate.trace.http.HttpTrace;
 import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.stereotype.Repository;
@@ -9,13 +11,15 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Repository
 public class LoggingInMemoryHttpTraceRepository extends InMemoryHttpTraceRepository {
+	private Logger log = LoggerFactory.getLogger(this.getClass());
+	
     public void add(HttpTrace trace) {
         super.add(trace);
-		/*
-		 * log.info("Trace:" + ToStringBuilder.reflectionToString(trace));
-		 * log.info("Request:" +
-		 * ToStringBuilder.reflectionToString(trace.getRequest())); log.info("Response:"
-		 * + ToStringBuilder.reflectionToString(trace.getResponse()));
-		 */
+		
+		  log.info("Trace:" + ToStringBuilder.reflectionToString(trace));
+		  log.info("Request:" +
+		  ToStringBuilder.reflectionToString(trace.getRequest())); log.info("Response:"
+		  + ToStringBuilder.reflectionToString(trace.getResponse()));
+		 
     }
 }
